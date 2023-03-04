f1e5d8d305c72a66c0a268da4a386f2b73f3eab0
avmochalov <al.mochalov93@gmail.com>
2023-03-04 13:37:10 +0300

    HW-6 first 6 ex

---
+++ hw-6.js
@@ -0,0 +1,41 @@
+// Задание 1
+
+// let firstArray = [1, 5, 4, 10, 0, 3];
+// for (const el of firstArray) {
+// 	console.log(el);
+//     if (el == 10) break;
+// }
+
+// Задание 2
+// let secondArray = [1, 5, 4, 10, 0, 3];
+// secondArray.forEach((el, index) => {
+//     if (el == 4) {
+//         console.log(`${index}`);
+//     }
+// });
+
+// Задание 3
+// let thirdArray = [1, 3, 5, 10, 20];
+// thirdArray = thirdArray.join(' ');
+// console.log(thirdArray);
+
+// Задание 4
+// let fourthArray = [];
+// for (let i = 0; i < 3; i++) {
+//     fourthArray.push([1, 1, 1]);
+// }
+// console.log(fourthArray);
+
+// Задание 5 
+// let fifthArray = [1, 1, 1];
+// for (let i = 0; i < 3; i++) {
+//     fifthArray.push(2);
+// }
+// console.log(fifthArray);
+
+// Задание 6
+let sixthArray = [9, 8, 7, 'a', 6, 5];
+sixthArray.sort();
+sixthArray.pop();
+sixthArray.reverse();
+console.log(sixthArray);
